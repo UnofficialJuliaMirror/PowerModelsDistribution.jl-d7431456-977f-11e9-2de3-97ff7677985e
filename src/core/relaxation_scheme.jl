69c5b4c946569641bed713b4656131ebd0ac2b81
Geth, Frederik (Energy, Newcastle) <Frederik.Geth@csiro.au>
2019-11-14 09:17:24 +1100

    experimental code on sdp->soc and sdp-r1->nlp

--- src/core/relaxation_scheme.jl
+++ src/core/relaxation_scheme.jl
@@ -176,3 +176,198 @@ function relaxation_psd_to_psd_real(m, mxreal, mximag; ndim=3)
         JuMP.@constraint(m, [mr -mi; mi mr] in JuMP.PSDCone())
     end
 end
+
+"""
+This constraints models
+M = [ Ar+im*Ai    Cr+im*Ci
+     (Cr+im*Ci)'  Br+im*Bi]
+M in PSDCone, rank(M) = 1
+as a set of polynomial matrix equations
+(Cr+im*Ci) (Cr+im*Ci)' = tr(Br)(Ar+im*Ai)
+(Cr+im*Ci)'(Cr+im*Ci)  = tr(Ar)(Br+im*Bi)
+"""
+function reformulation_psd_rank1_to_quadratic(model, Ar, Ai, Br, Bi, Cr, Ci)
+    @assert size(Ar) == size(Ai)
+    @assert size(Br) == size(Bi)
+    @assert size(Cr) == size(Ci)
+    @assert size(Cr,1) == size(Ar,1)
+    @assert size(Cr,2) == size(Br,2)
+
+    # n = size(Ar,1)
+    # m = size(Br,1)
+    #
+    # utridiagA   = [(i,j) for i=1:n, j=1:n if i<=j] # upper triangle + diagonal elements of A
+    # utriA       = [(i,j) for i=1:n, j=1:n if i< j] # upper triangle elements of A
+    #
+    # utridiagB   = [(i,j) for i=1:m, j=1:m if i<=j] # upper triangle + diagonal elements of B
+    # utriB       = [(i,j) for i=1:m, j=1:m if i< j] # upper triangle elements of B
+    #
+    # for (a,b) in utridiagA
+    #     JuMP.@constraint(model, sum(Cr[a,j]*Cr[b,j] + Ci[a,j]*Ci[b,j] for j in 1:m) == Ar[a,b] * sum(Br[j,j] for j in 1:m))
+    # end
+    #
+    # for (a,b) in utriA
+    #     JuMP.@constraint(model, sum(Ci[a,j]*Cr[b,j] - Cr[a,j]*Ci[b,j] for j in 1:m) == Ai[a,b] * sum(Br[j,j] for j in 1:m))
+    # end
+    #
+    # for (a,b) in utridiagB
+    #     JuMP.@constraint(model, sum(Cr[i,a]*Cr[i,b] + Ci[i,a]*Ci[i,b] for i in 1:n) == Br[a,b] * sum(Ar[i,i] for i in 1:n))
+    # end
+    #
+    # for (a,b) in utriB
+    #     JuMP.@constraint(model, sum(Ci[i,a]*Cr[i,b] - Cr[i,a]*Ci[i,b] for i in 1:n) == Bi[a,b] * sum(Ar[i,i] for i in 1:n))
+    # end
+
+
+    # (Cr+im*Ci) (Cr+im*Ci)' = tr(Br)(Ar+im*Ai)
+    matrix_product_real(model, Ar, Br, Cr, Ci)
+    matrix_product_imag(model, Ai, Br, Cr, Ci)
+
+    # equations are redundant when size(Cr) == (1,1)
+    if size(Cr) != (1, 1)
+        # (Cr+im*Ci)'(Cr+im*Ci)  = tr(Ar)(Br+im*Bi)
+        # swap A and B, take complex conjugate transpose of C
+        matrix_product_real(model, Br, Ar, Cr', -Ci')
+        matrix_product_imag(model, Bi, Ar, Cr', -Ci')
+    end
+end
+
+"""
+This constraints models
+M = Mr + im*Mi in PSDCone, rank(M) = 1
+as a set of quadratic matrix equations by partitioning M in four equal quadrants
+Ar+im*Ai the upper diagonal block
+Br+im*Bi the lower diagonal block
+Cr+im*Ci the upper right block
+
+and then calls reformulation_psd_rank1_to_quadratic(model, Ar, Ai, Br, Bi, Cr, Ci)
+"""
+function reformulation_psd_rank1_to_quadratic(model, Mr, Mi)
+    @assert size(Mr) == size(Mi)
+    @assert size(Mr,1) == size(Mr,2)
+    @assert iseven(size(Mr,1))
+
+    n = Int(size(Mr,1)/2)
+
+    Ar = Mr[1:n,    1:n]
+    Ai = Mi[1:n,    1:n]
+    Br = Mr[n+1:2n, n+1:2n]
+    Bi = Mi[n+1:2n, n+1:2n]
+    Cr = Mr[1:n,    n+1:2n]
+    Ci = Mi[1:n,    n+1:2n]
+
+    reformulation_psd_rank1_to_quadratic(model, Ar, Ai, Br, Bi, Cr, Ci)
+end
+
+"""
+This constraints models the matrix equation
+(Cr)(Cr)' + (Ci)(Ci)' = tr(Br)(Ar)
+as a set of scalar equations.
+The lower triangular elements generate redundant constraints due to symmetry, and are therefore not constructed
+"""
+function matrix_product_real(model, Ar, Br, Cr, Ci)
+    n = size(Cr,1)
+    m = size(Cr,2)
+    utridiag   = [(i,j) for i=1:n, j=1:n if i<=j] # upper triangle + diagonal elements of A
+
+    for (a,b) in utridiag
+        JuMP.@constraint(model, sum(Cr[a,j]*Cr[b,j] + Ci[a,j]*Ci[b,j] for j in 1:m) == Ar[a,b] * sum(Br[j,j] for j in 1:m))
+    end
+end
+
+"""
+This constraints models the matrix equation
+(Ci)(Cr)' - (Cr)(Ci)' = tr(Br)(Ai)
+as a set of scalar equations.
+The lower triangular and diagonal elements generate redundant constraints due to symmetry, and are therefore not constructed
+"""
+function matrix_product_imag(model, Ai, Br, Cr, Ci)
+    n = size(Cr,1)
+    m = size(Cr,2)
+    utri       = [(i,j) for i=1:n, j=1:n if i< j] # upper triangle elements of A
+
+    for (a,b) in utri
+        JuMP.@constraint(model, sum(Ci[a,j]*Cr[b,j] - Cr[a,j]*Ci[b,j] for j in 1:m) == Ai[a,b] * sum(Br[j,j] for j in 1:m))
+    end
+end
+
+
+function sdp_to_soc_kim_kojima(model, Are, Aim, are, aim, alphare, Ure, Uim; tol=1e-8)
+    @assert size(Are) == size(Aim)
+    @assert size(are) == size(aim)
+
+    @assert size(Are)[1] == size(are)[1]
+
+    Ure[abs.(Ure).<=tol] .=0
+    Uim[abs.(Uim).<=tol] .=0
+
+    U = Ure+im*Uim
+    C = U*U'
+    Cre = real(C)
+    Cim = imag(C)
+    Cre[abs.(Cre).<=tol] .=0
+    Cim[abs.(Cim).<=tol] .=0
+
+    @assert size(Cre) == size(Are)
+
+    rhs_1 = alphare
+    rhs_2 = sum(Cre.*Are) + sum(Cim.*Aim)
+
+    lhs_re = Ure'* are + Uim'* aim
+    lhs_im = Ure'* aim - Uim'* are
+    @show rhs_1, rhs_2
+    @show lhs_re, lhs_im
+
+    JuMP.@constraint(model, rhs_2 >= 0)
+    JuMP.@constraint(model,     [rhs_1+rhs_2;
+                                 rhs_1-rhs_2;
+                                 2*lhs_re;
+                                 2*lhs_im] in JuMP.SecondOrderCone())
+
+end
+
+#
+# function sdp_to_soc_kim_kojima(model, Mre, Mim, theta)
+#     @assert size(Mre) == size(Mim) == (3,3)
+#
+#     Are = Mre[[1;2;3], [1;2;3]]
+#     Aim = Mim[[1;2;3], [1;2;3]]
+#
+#     # sdp_to_soc_kim_kojima(model, Are, Aim, are, aim, alphare, Ure, Uim)
+#
+#     CA = Are[1,1] + Are[2,2] + 2*(cos(theta)*Are[1,2] - sin(theta)*Aim[1,2] )
+#     UHare = Are[1,3] + cos(theta)*Are[2,3] + sin(theta)*Aim[2,3]
+#     UHaim = Aim[1,3] + cos(theta)*Aim[2,3] - sin(theta)*Are[2,3]
+#
+#     JuMP.@constraint(model,     [Are[3,3] + CA;
+#                                  Are[3,3] - CA;
+#                                  2*UHare;
+#                                  2*UHaim] in JuMP.SecondOrderCone())
+#
+#
+#     Are = Mre[[2;3;1], [2;3;1]]
+#     Aim = Mim[[2;3;1], [2;3;1]]
+#     # sdp_to_soc_kim_kojima(model, Are, Aim, are, aim, alphare, Ure, Uim)
+#
+#     CA = Are[1,1] + Are[2,2] + 2*(cos(theta)*Are[1,2] - sin(theta)*Aim[1,2] )
+#     UHare = Are[1,3] + cos(theta)*Are[2,3] + sin(theta)*Aim[2,3]
+#     UHaim = Aim[1,3] + cos(theta)*Aim[2,3] - sin(theta)*Are[2,3]
+#
+#     JuMP.@constraint(model,     [Are[3,3] + CA;
+#                                  Are[3,3] - CA;
+#                                  2*UHare;
+#                                  2*UHaim] in JuMP.SecondOrderCone())
+#
+#
+#     Are = Mre[[3;1;2], [3;1;2]]
+#     Aim = Mim[[3;1;2], [3;1;2]]
+#     # sdp_to_soc_kim_kojima(model, Are, Aim, are, aim, alphare, Ure, Uim)
+#     CA = Are[1,1] + Are[2,2] + 2*(cos(theta)*Are[1,2] - sin(theta)*Aim[1,2] )
+#     UHare = Are[1,3] + cos(theta)*Are[2,3] + sin(theta)*Aim[2,3]
+#     UHaim = Aim[1,3] + cos(theta)*Aim[2,3] - sin(theta)*Are[2,3]
+#
+#     JuMP.@constraint(model,     [Are[3,3] + CA;
+#                                  Are[3,3] - CA;
+#                                  2*UHare;
+#                                  2*UHaim] in JuMP.SecondOrderCone())
+# end
--- src/form/bf_mx_soc.jl
+++ src/form/bf_mx_soc.jl
@@ -24,13 +24,17 @@ function constraint_tp_model_current(pm::_PMs.GenericPowerModel{T}, n::Int, i, f
 
     relaxation_psd_to_soc(pm.model, mat_real, mat_imag, complex=true)
 
+    # reformulation_psd_rank1_to_quadratic(pm.model, w_fr_re, w_fr_im, ccm_re, ccm_im, p_s_fr, q_s_fr)
+
+    # reformulation_psd_rank1_to_quadratic(pm.model, mat_real, mat_imag)
     # code below useful for debugging: valid inequality equired to make the SOC-NLP formulation more accurate
     # (l,i,j) = f_idx
     # t_idx = (l,j,i)
     # p_to = _PMs.var(pm, n, :P_mx)[t_idx]
     # total losses are positive when g_fr, g_to and r are positive
     # not guaranteed for individual phases though when matrix obtained through Kron's reduction
-    # JuMP.@constraint(pm.model, tr(p_fr) + tr(p_to) >= 0)
+    # JuMP.@constraint(pm.model, sum(p_fr[i,i] for i in 1:size(p_fr,1)) + sum(p_to[i,i] for i in 1:size(p_to,1)) >= 0)
+
 end
 
 
@@ -59,4 +63,135 @@ function constraint_tp_model_current(pm::_PMs.GenericPowerModel{T}, n::Int, i, f
     ]
 
     relaxation_psd_to_soc_conic(pm.model, mat_real, mat_imag, complex=true)
+
+
+    # JuMP.@constraint(pm.model,
+    # [
+    # w_fr_re  -w_fr_im;
+    # w_fr_im   w_fr_re
+    # ] in JuMP.PSDCone())
+    #
+    # JuMP.@constraint(pm.model,
+    # [
+    # ccm_re  -ccm_im;
+    # ccm_im   ccm_re
+    # ] in JuMP.PSDCone())
+
+    # JuMP.@constraint(pm.model,
+    # [
+    # mat_real[2:4, 2:4]  -mat_imag[2:4, 2:4];
+    # mat_imag[2:4, 2:4]   mat_real[2:4, 2:4]
+    # ] in JuMP.PSDCone())
+    #
+    # JuMP.@constraint(pm.model,
+    # [
+    # mat_real[3:5, 3:5]  -mat_imag[3:5, 3:5];
+    # mat_imag[3:5, 3:5]   mat_real[3:5, 3:5]
+    # ] in JuMP.PSDCone())
+    #
+    # JuMP.@constraint(pm.model,
+    # [
+    # mat_real[[5;6;1], [5;6;1]]  -mat_imag[[5;6;1], [5;6;1]];
+    # mat_imag[[5;6;1], [5;6;1]]   mat_real[[5;6;1], [5;6;1]]
+    # ] in JuMP.PSDCone())
+
+    mixre = mat_real[2:4, 2:4]
+    mixim = mat_imag[2:4, 2:4]
+
+    mixre2 = mat_real[3:5, 3:5]
+    mixim2 = mat_imag[3:5, 3:5]
+
+    mixre3 = mat_real[[5;6;1], [5;6;1]]
+    mixim3 = mat_imag[[5;6;1], [5;6;1]]
+
+
+    steps = pm.setting["n_steps_KK"]
+    for theta= pm.setting["init_KK"]:(2*π/steps):(pm.setting["init_KK"]+2*π-0.001)
+        U = [1; pm.setting["tv"]* exp(im*theta)]
+        Ure = real(U)
+        Uim = imag(U)
+        @show U
+
+        Are = mat_real[1:5, 1:5]
+        Aim = mat_imag[1:5, 1:5]
+        are = mat_real[1:5,6]
+        aim = mat_imag[1:5,6]
+        alphare = mat_real[6,6]
+
+
+        #
+        # Ure = [1 1 1 1 1]'
+        # Uim = 0*[1 1 1 1 1]'
+        #
+        # using Combinatorics
+        # n = 6
+        # k = 5
+        # collect(combinations(1:n,k))
+        #
+        Mre = w_fr_re[[1;2;3], [1;2;3]]
+        Mim = w_fr_im[[1;2;3], [1;2;3]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+
+        Mre = w_fr_re[[2;3;1], [2;3;1]]
+        Mim = w_fr_im[[2;3;1], [2;3;1]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+
+
+        Mre = w_fr_re[[3;1;2], [3;1;2]]
+        Mim = w_fr_im[[3;1;2], [3;1;2]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+
+        Mre = ccm_re[[1;2;3], [1;2;3]]
+        Mim = ccm_im[[1;2;3], [1;2;3]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+
+        Mre = ccm_re[[2;3;1], [2;3;1]]
+        Mim = ccm_im[[2;3;1], [2;3;1]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+
+
+        Mre = ccm_re[[3;1;2], [3;1;2]]
+        Mim = ccm_im[[3;1;2], [3;1;2]]
+        Are = Mre[1:2, 1:2]
+        Aim = Mim[1:2, 1:2]
+        are = Mre[1:2,3]
+        aim = Mim[1:2,3]
+        alphare = Mre[3,3]
+        sdp_to_soc_kim_kojima(pm.model, Are, Aim, are, aim, alphare, Ure, Uim)
+        # Ure = [1 -1 1 -1 1]'
+        # Uim = 0*[1 1 1 1 1]'
+        # sdp_to_soc_kim_kojima(pm.model, w_fr_re,    w_fr_im,    theta)
+        #
+        # sdp_to_soc_kim_kojima(pm.model, mixre,    mixim,    theta)
+        # sdp_to_soc_kim_kojima(pm.model, mixre2,    mixim2,    theta)
+        # sdp_to_soc_kim_kojima(pm.model, mixre3,    mixim3,    theta)
+
+        # sdp_to_soc_kim_kojima(pm.model, ccm_re,     ccm_im,     theta)
+    end
+
 end
